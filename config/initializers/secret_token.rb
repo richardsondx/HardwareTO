5ba77a28570cb20b80d394eb2cd9ab83bb12a754
evenix <richardsondx@gmail.com>
2013-07-30 02:24:07 -0400

    intial commit with landing page ready

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hardwareto::Application.config.secret_token = '7f2560f542b7370f57b120ff6d1e6c70bbbc033734bf1917985bbe1d4634836b5c91ab09791ab1aed5a9b5a4207add141d5e1cf3e72683526cb297382eabde1a'
